a812bc42655d92436fad99a0c7b162a234addf07
peter <likaitian731@gmail.com>
2019-12-24 21:42:28 -0600

    update exceptions

---
+++ src/main/java/App/test/app/test/exceptions/UserServiceException.java
@@ -0,0 +1,12 @@
+package App.test.app.test.exceptions;
+
+public class UserServiceException extends RuntimeException{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -272163393057181347L;
+
+	public UserServiceException(String message){
+		super(message);
+	}
+}
--- src/main/java/App/test/app/test/ui/controller/userController.java
+++ src/main/java/App/test/app/test/ui/controller/userController.java
@@ -2,11 +2,14 @@ package App.test.app.test.ui.controller;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import App.test.app.test.exceptions.UserServiceException;
 import App.test.app.test.service.UserService;
 import App.test.app.test.shared.dto.UserDto;
 import App.test.app.test.ui.model.request.UserDetailsRequestModel;
+import App.test.app.test.ui.model.response.ErrorMessages;
 import App.test.app.test.ui.model.response.UserResponse;
 
 @RestController
@@ -15,7 +18,9 @@ public class userController {
 	@Autowired
 	UserService userService;
 	
-	@GetMapping(path = "/{id}")
+	@GetMapping(path = "/{id}",
+			produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}
+			)
 	public UserResponse getUser(@PathVariable String id) {
 		UserResponse returnValue = new UserResponse();
 		
@@ -26,9 +31,15 @@ public class userController {
 		return returnValue;
 	}
 	
-	@PostMapping
-	public UserResponse createUser(@RequestBody UserDetailsRequestModel userDetails) {
+	@PostMapping (
+			consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+			produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}
+			)
+	public UserResponse createUser(@RequestBody UserDetailsRequestModel userDetails) throws Exception{
 		UserResponse returnValue = new UserResponse();
+		
+		if(userDetails.getFirstName().isEmpty()) 
+			throw new UserServiceException(ErrorMessages.MISSING_REQUIRED_FIELD.getErrorMessage());
 		
 		UserDto userDto = new UserDto();
 		BeanUtils.copyProperties(userDetails, userDto);
